f16c07eaa2ab654d58103dfe2e725b6e13146b3a
Harrison Conlin <me@harrisony.com>
2018-11-10 09:53:42 +0100

    few more goodies

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 git clone https://github.com/harrisony/iLabX.git harrisons-ilab
+wget -c https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz
 cd harrisons-ilab
 git checkout 201802
 git pull
--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh 
 xhost + 
 sudo cp 99_tcp.conf /etc/lightdm/lightdm.conf.d/99_tcp.conf
+sudo cp /home/ilab/harrisons-ilab/dane* /root/
+sudo cp /home/ilab/go1.10.3.linux-amd64.tar.gz /root/
 
