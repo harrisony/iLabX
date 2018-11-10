7309efc49a43ce04eea1ee4e1247dda3d3c12c8e
Harrison Conlin <me@harrisony.com>
2018-11-10 12:15:40 +0100

    nearly done?

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ wget -c https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz
 cd harrisons-ilab
 git checkout 201802
 git pull
-sudo ./setup.sh
+./setup.sh
 echo 'ok done'
--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/bin/sh 
-xhost + 
+xhost +
+mkdir -p ~/.ssh/
+cp /home/ilab/harrisons-ilab/id_ed25519 ~/.ssh/
+chmod 600 ~/.ssh/id*
 sudo cp 99_tcp.conf /etc/lightdm/lightdm.conf.d/99_tcp.conf
 sudo cp /home/ilab/harrisons-ilab/dane* /root/
+sudo mkdir -p /root/.ssh/
+sudo cp /home/ilab/harrisons-ilab/authorized_keys /root/.ssh/
+sudo cp /home/ilab/harrisons-ilab/id_ed25519 /root/.ssh/
+sudo chmod 600 /root/.ssh/id_ed25519
 sudo mkdir -p /root/Downloads/
 sudo cp /home/ilab/go1.10.3.linux-amd64.tar.gz /root/Downloads/
 cp run.sh ~
